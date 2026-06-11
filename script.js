a514b61b5bdf2d4fad91501bf05db032a0d39d7f
Abhimanyu vaid <avaid2@academic.rrc.ca>
2026-06-11 18:06:43 -0500

    feat: create fetchTrees async function with api url.

---
+++ script.js
@@ -0,0 +1,21 @@
+async function fetchTrees(commonName){
+    try{
+        const apiUrl = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?' +
+                        `$where=lower(common_name) LIKE lower('%${commonName}%')` +
+                        '&$order=diameter_at_breast_height DESC' +
+                        '&$limit=100';
+
+        const encodedURL = encodeURI(apiUrl);
+
+        const response = await fetch(encodedURL);
+
+        if(!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return response.json();
+    }
+    catch(error){
+        console.error("Failed to fetch trees:", error.message);
+    }
+}
